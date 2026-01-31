1834d1534a47d9f948d1025c1a8947115f2da87b
Qcassino <qcassino01@gmail.com>
2026-01-31 00:25:08 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 import { auth, db } from "./firebase.js";
 import {
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
   signOut
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import {
