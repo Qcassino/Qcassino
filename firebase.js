a2ca7371c6cbe65d79ae9c9f61f8e157b9aaea99
Qcassino <qcassino01@gmail.com>
2026-02-01 09:43:36 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,30 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
-import { initializeApp } from
-"https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-import { getAuth } from
-"https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-import { getFirestore } from
-"https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey:"AIzaSyDImI1Y2f9fCicqXr4MZi4VrtSM2lJNHUM",
+  apiKey: "AIzaSyDImI1Y2f9fCicqXr4MZi4VrtSM2lJNHUM",
   authDomain: "qcassino-b40ff.firebaseapp.com",
   projectId: "qcassino-b40ff",
+  storageBucket: "qcassino-b40ff.firebasestorage.app",
+  messagingSenderId: "824724689710",
+  appId: "1:824724689710:web:2cf828f1b3bb53701460bf"
 };
 
+// 🚀 INICIALIZA
 export const app = initializeApp(firebaseConfig);
+
+// 🔐 AUTH COM PERSISTÊNCIA
 export const auth = getAuth(app);
+await setPersistence(auth, browserLocalPersistence);
+
+// 🗄️ FIRESTORE
 export const db = getFirestore(app);
-
-
-
-
