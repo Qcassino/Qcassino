aa8383f89b78cdc11125dc6dc53c4cc46568247f
Qcassino <qcassino01@gmail.com>
2026-02-01 09:59:25 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-
-import {
-  getAuth,
-  setPersistence,
-  browserLocalPersistence
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDImI1Y2f9fCicqXr4MZi4VrtSM2lJNHUM",
@@ -19,12 +11,6 @@ const firebaseConfig = {
   appId: "1:824724689710:web:2cf828f1b3bb53701460bf"
 };
 
-// 🚀 INICIALIZA
 export const app = initializeApp(firebaseConfig);
-
-// 🔐 AUTH COM PERSISTÊNCIA
 export const auth = getAuth(app);
-await setPersistence(auth, browserLocalPersistence);
-
-// 🗄️ FIRESTORE
 export const db = getFirestore(app);
